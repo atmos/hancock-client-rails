da0dfb036e379cc1cf82a3e63203072c1aeccf1e
Corey Donohoe <atmos@atmos.org>
2009-06-15 22:27:54 -0700

    make this work with 0.0.7 and the latest cucumber etc

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,9 @@
 # Sets up the Rails environment for Cucumber
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
+gem 'rspec'
+gem 'rspec-rails'
 require 'cucumber/rails/world'
-require 'cucumber/formatters/unicode' # Comment out this line if you don't want Cucumber Unicode support
 Cucumber::Rails.use_transactional_fixtures
 
 require 'webrat'
